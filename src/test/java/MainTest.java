a52df81dfde49a90139cc1cc0617bffd0d55c905
RaphiSmadja <raphael_smadja@hotmail.com>
2021-01-13 18:40:25 +0100

    Update unit Test :white_check_mark:

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -50,19 +50,61 @@ public class MainTest {
     public void adventure_should_do_not_exceed_the_limits_carte_4_4() {
         //Given
         String[] args = {"carteTest4-4.txt"};
+        int axeH = 4;
+        int axeV = 4;
         Main main = new Main();
         Mountain mountain = new Mountain(0,0);
         Adventurer adventurer = new Adventurer("Bob", 1,3,"S", "AAAAAAA");
         Object[][] actual ={
                 {mountain, ".", ".", "."},
-                {".", ".", ".", "."},
-                {".", ".", ".", "."},
+                {".", ".", ".", ".", "."},
+                {".", ".", ".", ".", "."},
                 {".", adventurer, ".", "."}
         };
         // When
         Object[][] expected = main.createGame(args);
 
         //Then
-        assertTrue(actual[1][3].equals(expected[1][3]));
+        assertAll("test all carte",
+                () -> assertEquals(actual.length, axeH),
+                () -> assertEquals(actual[0].length, axeV),
+                () -> assertTrue(actual[1][3].equals(expected[1][3])),
+                () -> assertTrue(actual[0][0].equals(expected[0][0])),
+                () -> assertTrue(actual[3][3].equals(expected[3][3]))
+        );
+
+    }
+    @Test
+    public void adventure_should_not_pass_mountain() {
+        // Given
+        String[] args = {"carteTest5-5.txt"};
+        int axeH = 5;
+        int axeV = 5;
+        Main main = new Main();
+        Mountain mountain = new Mountain(1,4);
+        Treasure treasure = new Treasure(1,2,1);
+        Adventurer adventurer = new Adventurer("Lea", 1,3,"N", "AAAAAGG");
+        adventurer.setNbTreasures(1);
+        Object[][] actual ={
+                {".", ".", ".", ".", "."},
+                {".", ".", ".", ".", "."},
+                {".", treasure, ".", ".", "."},
+                {".", adventurer, ".", ".", "."},
+                {".", mountain, ".", ".", "."}
+        };
+        // When
+        Object[][] expected = main.createGame(args);
+
+        // Then
+        Adventurer adventurerExpected = (Adventurer) actual[3][1];
+        assertAll("test all carte and adventurer",
+                () -> assertEquals(actual.length, axeH),
+                () -> assertEquals(actual[0].length, axeV),
+                () -> assertEquals(actual[2][1],treasure),
+                () -> assertTrue(adventurerExpected.getOrientation().equals("N")),
+                () -> assertEquals(adventurerExpected.getNbTreasures(),1),
+                () -> assertTrue(actual[0][0].equals(expected[0][0])),
+                () -> assertTrue(actual[1][2].equals(expected[1][2]))
+        );
     }
 }
